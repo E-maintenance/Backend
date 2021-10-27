446cf79a69b162a7effe134a45c1f1f6f68d8d71
naeemalomari <naeemalomari96@gmail.com>
2021-10-27 20:48:27 +0300

    conflict

--- src/main/java/runtimeTerror/autoCare/controller/ContactUs/ContactUsCont.java
+++ src/main/java/runtimeTerror/autoCare/controller/ContactUs/ContactUsCont.java
@@ -1,34 +1,34 @@
-package runtimeTerror.autoCare.controller.ContactUs;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.util.HtmlUtils;
-import runtimeTerror.autoCare.model.ContactUs.ContactUsModel;
-import runtimeTerror.autoCare.model.ContactUs.HelloMessage;
-import runtimeTerror.autoCare.repository.ContactUs.ContactUsRep;
-import runtimeTerror.autoCare.repository.ContactUs.HelloMessageRepository;
-
-import java.net.URISyntaxException;
-
-@Controller
-public class ContactUsCont {
-    @Autowired
-    ContactUsRep ContactUs;
-    @Autowired
-    HelloMessageRepository helloMessageRepository;
-    @MessageMapping("/chat")
-    @SendTo("/topic/contacts")
-    public ContactUsModel contact(HelloMessage message) throws InterruptedException, URISyntaxException {
-
-        helloMessageRepository.save(message);
-        Thread.sleep(1000); // simulated delay
-
-        ContactUsModel greeting = new ContactUsModel(HtmlUtils.htmlEscape(message.getName()));
-        System.out.println(HtmlUtils.htmlEscape(message.getName()));
-        ContactUs.save(greeting);
-        return greeting;
-    }
-
-}
+//package runtimeTerror.autoCare.controller.ContactUs;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.handler.annotation.SendTo;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.util.HtmlUtils;
+//import runtimeTerror.autoCare.model.ContactUs.ContactUsModel;
+//import runtimeTerror.autoCare.model.ContactUs.HelloMessage;
+//import runtimeTerror.autoCare.repository.ContactUs.ContactUsRep;
+//import runtimeTerror.autoCare.repository.ContactUs.HelloMessageRepository;
+//
+//import java.net.URISyntaxException;
+//
+//@Controller
+//public class ContactUsCont {
+//    @Autowired
+//    ContactUsRep ContactUs;
+//    @Autowired
+//    HelloMessageRepository helloMessageRepository;
+//    @MessageMapping("/chat")
+//    @SendTo("/topic/contacts")
+//    public ContactUsModel contact(HelloMessage message) throws InterruptedException, URISyntaxException {
+//
+//        helloMessageRepository.save(message);
+//        Thread.sleep(1000); // simulated delay
+//
+//        ContactUsModel greeting = new ContactUsModel(HtmlUtils.htmlEscape(message.getName()));
+//        System.out.println(HtmlUtils.htmlEscape(message.getName()));
+//        ContactUs.save(greeting);
+//        return greeting;
+//    }
+//
+//}
--- src/main/java/runtimeTerror/autoCare/webSecurityConfig/ContactUsConfig/ContactUsContConfig.java
+++ src/main/java/runtimeTerror/autoCare/webSecurityConfig/ContactUsConfig/ContactUsContConfig.java
@@ -1,25 +1,25 @@
-package runtimeTerror.autoCare.webSecurityConfig.ContactUsConfig;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-
-@Configuration
-@EnableWebSocketMessageBroker
-public class ContactUsContConfig implements WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-
-        registry.enableSimpleBroker("/topic");
-        registry.setApplicationDestinationPrefixes("/app");
-    }
-
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/gs-guide-websocket").withSockJS();
-
-    }
-}
+//package runtimeTerror.autoCare.webSecurityConfig.ContactUsConfig;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+//import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+//import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+//import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+//
+//@Configuration
+//@EnableWebSocketMessageBroker
+//public class ContactUsContConfig implements WebSocketMessageBrokerConfigurer {
+//
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry registry) {
+//
+//        registry.enableSimpleBroker("/topic");
+//        registry.setApplicationDestinationPrefixes("/app");
+//    }
+//
+//
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/gs-guide-websocket").withSockJS();
+//
+//    }
+//}
